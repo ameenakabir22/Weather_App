3852412f43be58165aae5b84af55b2891587e521
Amina_Kabir <ameenakabir22@gmail.com>
2023-10-25 23:33:56 +0000

    i created funcrions for getting the location of a user

--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -1,3 +1,4 @@
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'package:weather_app/model/weathermodel.dart';
@@ -19,5 +20,14 @@ class WeatherServices {
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
     }
+    // fetch current location
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    // convert the location into a list placemark of objects
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+    // extract the city name from the first placemark
+    String? city = placemarks[0].locality;
+    return city ?? "";
   }
 }
