b3bc76973bb52fe8ba70c7ab88eb5bf5f2c63cda
Amina_Kabir <ameenakabir22@gmail.com>
2023-10-26 00:32:22 +0000

    still on creating a method for accessing a user's location

--- lib/pages/weatherpage.dart
+++ lib/pages/weatherpage.dart
@@ -1,7 +1,46 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class WeatherPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:weather_app/model/weathermodel.dart';
+import 'package:weather_app/services/weather_services.dart';
+
+class WeatherPage extends StatefulWidget {
   const WeatherPage({Key? key}) : super(key: key);
+
+  @override
+  _WeatherPageState createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  // api key
+  final _weatherService = WeatherServices('YOUR_API_KEY_GOES_HERE');
+  Weather? _weather;
+
+  // fetch weather
+  Future_fetchWeather() async {
+    // get the current location
+    String cityName = await _weatherService.getCurrentCity();
+
+    // get weather for city
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    }
+    //errors
+    catch (e) {
+      print(e);
+    }
+  }
+
+  //animations
+
+  // init state
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
