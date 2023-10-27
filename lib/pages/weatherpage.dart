1666c0776c5156f8d81ce02d6bec62caa17aed04
Amina_Kabir <ameenakabir22@gmail.com>
2023-10-27 19:47:25 +0000

    i completed the functions for getting current weather and location

--- lib/pages/weatherpage.dart
+++ lib/pages/weatherpage.dart
@@ -1,49 +1,53 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:weather_app/model/weathermodel.dart';
 import 'package:weather_app/services/weather_services.dart';
+import 'package:weather_app/model/weathermodel.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({Key? key}) : super(key: key);
 
   @override
-  _WeatherPageState createState() => _WeatherPageState();
+  State<WeatherPage> createState() => _WeatherPageState();
 }
 
 class _WeatherPageState extends State<WeatherPage> {
   // api key
-  final _weatherService = WeatherServices('YOUR_API_KEY_GOES_HERE');
+  final _weatherService = WeatherServices('63129efc2865e23187009d071fdcb739');
   Weather? _weather;
-
   // fetch weather
-  Future_fetchWeather() async {
-    // get the current location
-    String cityName = await _weatherService.getCurrentCity();
+  _fetchWeather() async {
+    // Get the current city
+    String? cityName = await _weatherService.getCurrentCity();
 
-    // get weather for city
-    try {
-      final weather = await _weatherService.getWeather(cityName);
-      setState(() {
-        _weather = weather;
-      });
-    }
-    //errors
-    catch (e) {
-      print(e);
+    if (cityName != null) {
+      // Get the weather for the city
+      try {
+        final weather = await _weatherService.getWeather(cityName);
+        // Assign the weather data to your _weather property if needed
+        setState(() {
+          _weather = weather;
+        });
+      } catch (error) {
+        // Handle any errors that occur during weather retrieval
+        print("Error fetching weather: $error");
+      }
+    } else {
+      // Handle the case where cityName is null (e.g., show an error or provide a default city name)
+      print("City name is null");
     }
   }
 
-  //animations
+  //weather animation
 
   // init state
   @override
   void initState() {
     super.initState();
+    // fetch weather on startup
+    _fetchWeather();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold();
   }
 }
